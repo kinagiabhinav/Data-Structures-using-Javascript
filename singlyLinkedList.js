d21df2eb9de03703a8c16d566b59ec4aa5432240
Abhinav K <kinagiabhinav@gmail.com>
2021-11-28 11:14:11 +0530

    added additional methods

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -99,10 +99,97 @@ class LinkedList {
 
         return node.data;
     }
+
+    //Method to get the node in the list
+    get(position){
+        //check if list is empty
+        if(position < 0 || position > this.length) return undefined;
+
+        let node = this.head;
+        let count = 0;
+        while(position !== count){
+            node = node.next;
+            count++;
+        }
+
+        return node;
+    }
+
+    //Method to set the node in the list
+    set(position, data){
+        //check if list is empty
+        let node = this.get(position);
+        if(!node) return undefined;
+
+        node.data = data;
+
+        return this;
+    }
+
+    //Method to insert data at given position
+    insertAt(position, data){
+        if(!position) this.unshift(data);
+        else if(position === this.length) this.push(data);
+        else {
+            let pre = this.get(position-1);
+            let post = this.get(position);
+
+            if(!pre || !post){
+                return false;
+            }
+            let node = new Node(data);
+            pre.next = node;
+            node.next = post;
+            this.length++;
+        }
+        return this;   
+    }
+
+    //Method to remove node from given position
+    removeFrom(position){
+        if(!position) this.shift();
+        else if(position === this.length - 1) this.pop();
+        else {
+            let pre = this.get(position-1);
+            let current = this.get(position);
+
+            if(!pre || !current){
+                return false;
+            }
+            pre.next = current.next;
+            current.next = null;
+            this.length--;
+        }
+        return this;   
+    }
+
+    reverse(){
+        let node = this.head;
+        this.head = this.tail;
+        this.tail = node;
+
+        let pre = null;
+        for (let index = 0; index < this.length; index++) {
+            let nextNode = node.next;
+            node.next = pre;
+            pre = node;
+            node = nextNode;
+        }
+
+        return this;
+    }
+
 }
 
 let list = new LinkedList();
 list.push(1);
 list.unshift(0);
-
+list.push(2);
+list.push(3);
+list.push(4);
+list.push(5);
+list.pop();
+list.shift();
+list.get(1);
+list.reverse();
 
